b1d40d9b73d1ce09c46389936acdc4b88ed17bfb
crombieplex <jadam310@cougarmail.cccc.edu>
2022-03-28 15:10:28 -0400

    Update userSchema.js

--- Schemas/userSchema.js
+++ Schemas/userSchema.js
@@ -5,7 +5,7 @@ const users = "users";
 const userSchema = new Schema({
   name: String,
   discordId: Number,
-  totalDonated: Number,
+  totalWithdrawal: Number,
 });
 
 const user_model = mongoose.model(users, userSchema);
